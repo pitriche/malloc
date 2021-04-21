4bfc44ed637073988f5f9b4adee8fb484fa808b2
pitriche <pitriche@student.42.fr>
2021-04-21 12:07:46 +0200

    linux norming ft_putnbr.c

--- libft/ft_putnbr.c
+++ libft/ft_putnbr.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunomartin <brunomartin@student.42.fr>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 14:40:28 by pitriche          #+#    #+#             */
-/*   Updated: 2021/04/20 22:05:14 by brunomartin      ###   ########.fr       */
+/*   Updated: 2021/04/21 12:06:04 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-inline static void	_putch(char c) { write(1, &c, 1); }
+inline static void	_putch(int c) { write(1, &c, 1); }
 
 static void			_rec_putnbr(unsigned long absolute)
 {
 	if (absolute >= 10)
 		_rec_putnbr(absolute / 10);
-	_putch(absolute % 10 + '0');
+	_putch((int)(absolute % 10) + '0');
 }
 
 void				ft_putnbr(long nb)
